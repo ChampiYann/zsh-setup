faf424e9aad0d46b168719bac8e25eb13b2de99a
Yan Rosema <yann.rosema@hotmail.com>
2021-10-21 11:25:09 +0200

    added sudo to tee commands

--- wsl-set-default-user.sh
+++ wsl-set-default-user.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # This script sets the cuirrent user as default user when starting a new wsl shell.
+echo "Current user is: $USER"
+$_USER = $USER
 if [[ "$EUID" -ne 0 ]]; then
-    echo "Current user is: $USER"
     ## Update wsl.conf with default user
     echo "Writing wsl.conf..."
-    echo "[user]" | tee -a /etc/wsl.conf
-    echo "default=$SUER" | tee -a /etc/wsl.conf
+    echo "[user]" | sudo tee -a /etc/wsl.conf
+    echo "default=$_USER" | sudo tee -a /etc/wsl.conf
 fi
