9eab955f8041cf16effc633a5d2451de5caf7472
Yan Rosema <yann.rosema@hotmail.com>
2021-10-21 11:26:31 +0200

    fix variable assignment

--- wsl-set-default-user.sh
+++ wsl-set-default-user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This script sets the cuirrent user as default user when starting a new wsl shell.
 echo "Current user is: $USER"
-$_USER = $USER
+_USER=$USER
 if [[ "$EUID" -ne 0 ]]; then
     ## Update wsl.conf with default user
     echo "Writing wsl.conf..."
