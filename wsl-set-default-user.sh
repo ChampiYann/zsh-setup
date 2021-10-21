c2dfbebed7490078086044fb7041234076dc9e77
Yan Rosema <yann.rosema@hotmail.com>
2021-10-21 11:18:46 +0200

    show current user when setting default user

--- wsl-set-default-user.sh
+++ wsl-set-default-user.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # This script sets the cuirrent user as default user when starting a new wsl shell.
 if [[ "$EUID" -ne 0 ]]; then
+    echo "Current user is: $USER"
     ## Update wsl.conf with default user
     echo "Writing wsl.conf..."
     echo "[user]" | tee -a /etc/wsl.conf
