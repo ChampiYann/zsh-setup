73c7f8197985c9ff8abd33150ef329a4c56f4919
Yann Rosema <yann.rosema@hotmail.com>
2021-10-25 15:20:51 +0200

    fix merge remains

--- .setup-scripts/wsl-network-setup.sh
+++ .setup-scripts/wsl-network-setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ## Get host machine ip from original resolv.conf
 echo "Grep original nameserver/host machine IP"
 ORIGINAL_NS=$(grep "nameserver" /etc/resolv.conf)
@@ -13,6 +15,7 @@ unlink /etc/resolv.conf
 
 ## add new nameservers to resolv.conf
 echo "Writing new resolv.conf..."
+echo "nameserver 1.1.1.1" | tee -a /etc/resolv.conf # add external nameserver first to speed up internet connectivity
+# (I noticed a significant improvement in respon time from the azure cli after switching the line with the next)
 echo $ORIGINAL_NS | tee -a /etc/resolv.conf # add host ip as first argument
-echo "nameserver 1.1.1.1" | tee -a /etc/resolv.conf
 echo "nameserver 8.8.8.8" | tee -a /etc/resolv.conf
